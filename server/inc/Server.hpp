9bde82748aeb51de846cc506adc7bbe0324a4daf
Xantass <theo.fouque@epitech.eu>
2023-12-07 14:54:47 +0100

    [FIX] add comment into .hpp

--- server/inc/Server.hpp
+++ server/inc/Server.hpp
@@ -25,16 +25,12 @@
 #include <unistd.h>
 #include <sys/select.h>
 
-/**
- * @brief This is the Server class
- * @details This class is to be deleted when the project is started
- * 
- */
 class Server {
 public:
     /**
      * @brief Construct a new Server object
      * 
+     * @param port The port number to bind the server to
      */
     Server(size_t port);
 
@@ -44,20 +40,47 @@ public:
      */
     ~Server();
 
+    /**
+     * @brief Starts the server
+     * 
+     * @return true if the server starts successfully, false otherwise
+     */
     bool start();
+
+    /**
+     * @brief Closes all client sockets
+     * 
+     */
     void closeSockets();
+
+    /**
+     * @brief Initializes the server socket
+     * 
+     */
     void initSocket();
+
+    /**
+     * @brief Main loop for the server to handle client connections
+     * 
+     * @return true if the loop runs successfully, false otherwise
+     */
     bool loop();
+
+    /**
+     * @brief Receives data from a specific client
+     * 
+     * @param client The client socket to receive data from
+     */
     void receiveData(int client);
 
 protected:
 private:
-    int _serverSocket;
-    int _port;
-    int _maxSocket;
-    sockaddr_in _serverAddr{};
-    std::vector<int> _clientSockets;
-    fd_set _readfds;
+    int _serverSocket; /**< The server's socket file descriptor */
+    int _port; /**< The port number the server is listening on */
+    int _maxSocket; /**< The maximum number of sockets */
+    sockaddr_in _serverAddr{}; /**< Server address information */
+    std::vector<int> _clientSockets; /**< Vector to store client sockets */
+    fd_set _readfds; /**< File descriptor set for select() */
 };
 
 #endif /* !SERVER_HPP_ */
