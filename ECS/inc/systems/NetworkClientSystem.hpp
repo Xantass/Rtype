2f21428be1032cd3ec2fbbda5febb1604cba7103
Xantass <theo.fouque@epitech.eu>
2023-12-20 09:13:56 +0100

    [ADD] pos event

--- ECS/inc/systems/NetworkClientSystem.hpp
+++ ECS/inc/systems/NetworkClientSystem.hpp
@@ -13,7 +13,17 @@
 #ifndef NETWORKCLIENTSYSTEM_HPP_
 #define NETWORKCLIENTSYSTEM_HPP_
 
-#include <asio.hpp>
+#if defined(_WIN32)           
+	#define NOGDI             // All GDI defines and routines
+	#define NOUSER            // All USER defines and routines
+#endif
+
+#include <asio.hpp> // or any library that uses Windows.h
+
+#if defined(_WIN32)           // raylib uses these names as function parameters
+	#undef near
+	#undef far
+#endif
 #include "System.hpp"
 #include "Coordinator.hpp"
 #include "components/Position.hpp"
@@ -77,19 +87,19 @@ public:
      * @brief Handles the received command data.
      * @param decodedIntegers The vector of integers representing the command data.
      */
-    void handleCmd(std::vector<int>& decodedIntegers);
+    void handleCmd(std::vector<int>& decodedIntegers, Coordinator &coordinator);
 
     /**
      * @brief Sends a ping request.
      * @param decodedIntegers The vector of integers for ping request data.
      */
-    void ping(std::vector<int>& decodedIntegers);
+    void ping(std::vector<int>& decodedIntegers, Coordinator &coordinator);
 
     /**
      * @brief Sends position data.
      * @param decodedIntegers The vector of integers for position data.
      */
-    void pos(std::vector<int>& decodedIntegers);
+    void pos(std::vector<int>& decodedIntegers, Coordinator &coordinator);
 
     /**
      * @brief Creates entities based on decoded data.
@@ -109,7 +119,7 @@ private:
     io_context _service; /**< The Boost ASIO io_service. */
     udp::socket _socket = udp::socket(_service, udp::endpoint(udp::v4(), findValidPort(_service))); /**< The UDP socket for communication. */
     udp::endpoint _serverEndpoint; /**< The endpoint of the server. */
-    std::function<void(std::vector<int>&)> _functions[8]; /**< Array of function pointers. */
+    std::function<void(std::vector<int>&, Coordinator &coordinator)> _functions[8]; /**< Array of function pointers. */
     int _id; /**< The ID of the client. */
 };
 
