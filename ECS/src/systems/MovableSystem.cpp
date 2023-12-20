6f30f75b9dbb061ee30f249c01b2dc8fada24ab2
Xantass <theo.fouque@epitech.eu>
2023-12-20 01:51:55 +0100

    [FIX] conflic with window

--- ECS/src/systems/MovableSystem.cpp
+++ ECS/src/systems/MovableSystem.cpp
@@ -6,7 +6,17 @@
 */
 
 #include "systems/MovableSystem.hpp"
-#include "raylib.h"
+#if defined(_WIN32)           
+	#define NOGDI             // All GDI defines and routines
+	#define NOUSER            // All USER defines and routines
+#endif
+
+#include "raylib.h" // or any library that uses Windows.h
+
+#if defined(_WIN32)           // raylib uses these names as function parameters
+	#undef near
+	#undef far
+#endif
 
 void MovableSystem::Update(Coordinator &coordinator)
 {
