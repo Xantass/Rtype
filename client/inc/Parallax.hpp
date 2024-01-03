41c7f3c6e68d67b757dedce96974c43e8617f0af
Lucas_lgd02 <lucas.guiard@epitech.eu>
2024-01-03 17:01:19 +0100

    [ADD] Documentation on Parallax class

--- client/inc/Parallax.hpp
+++ client/inc/Parallax.hpp
@@ -5,30 +5,62 @@
 ** Parallax
 */
 
+/**
+ * @file Parallax.hpp
+ * @brief This is the file for the Parallax class
+ *
+ */
 #ifndef PARALLAX_HPP_
 #define PARALLAX_HPP_
 
-#include <tuple>
 #include <vector>
-#include <map>
 #include <iostream>
 #include <filesystem>
 #include "Graphic.hpp"
 
+/**
+ * @brief This is the Parallax class
+ *
+ */
 class Parallax {
     public:
+        /**
+         * @brief Construct a new Parallax object
+         *
+         * @param pathToAssets
+         */
         Parallax(std::string pathToAssets);
+        /**
+         * @brief Destroy the Parallax object
+         *
+         */
         ~Parallax();
+        /**
+         * @brief Draw all textures of the parallax
+         *
+         */
         void draw(void);
 
     protected:
     private:
-        std::map<std::string, float> _speedMap;
-        std::map<std::string, Texture2D> _textureMap;
         std::vector<std::tuple<Texture2D, float>> _textureList;
         std::string _path;
+        /**
+         * @brief Load all textures from parameter
+         *
+         * @param paths
+         */
         void loadTextures(std::vector<std::string> paths);
+        /**
+         * @brief Get all files in folder
+         *
+         * @return std::vector<std::string>
+         */
         std::vector<std::string> getAllFiles(void);
+        /**
+         * @brief Update the position of the textures
+         *
+         */
         void update(void);
 };
 
