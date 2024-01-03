2dc6d4a58ab35dd799a926ac054da3544e59d52a
Adrien Le Corvec <adrien.le-corvec@epitech.eu>
2024-01-03 16:24:40 +0100

    [ADD] Entity can't have more hp than the max

--- ECS/src/systems/HealthSystem.cpp
+++ ECS/src/systems/HealthSystem.cpp
@@ -12,8 +12,10 @@ void HealthSystem::Update(Coordinator &coordinator)
     for (auto entity : this->_entities) {
         auto& hp = coordinator.GetComponent<HealthPoint>(entity);
         if (hp._curr_hp <= 0) {
-            //Destroy Entity
+            //send Event : Destroy entity [ID: entity]
             std::cout << entity << " has 0 hp remaining" << std::endl;
         }
+        if (hp._curr_hp >= hp._max_hp)
+            hp._curr_hp = hp._max_hp;
     }
 }
