a6a09f18e74a32a526c2ec84b011a537a648069d
Lucas_lgd02 <lucas.guiard@epitech.eu>
2024-01-06 17:27:12 +0100

    [FIX] change key for menu, remove infinite loop in menu

--- client/inc/Menu.hpp
+++ client/inc/Menu.hpp
@@ -26,7 +26,7 @@ class Menu {
         Menu(std::string host, std::string port, std::string name) : _host(host), _port(port), _name(name), _nbPlayer("4") {}
         ~Menu() {}
         void displayMenu(void) {
-            if (Graphic::isKeyPressed(KEY_BACKSPACE)) {
+            if (Graphic::isKeyPressed(KEY_TAB)) {
                 action = "";
             }
             if (action == "<") {
@@ -71,7 +71,7 @@ class Menu {
             displayTextInput({760, 500}, {400, 80}, _port);
             displayTextInput({760, 620}, {400, 80}, _name);
             displayTextInput({760, 740}, {180, 80}, _nbPlayer);
-            displayButton({980, 740}, {180, 80}, "Create Room", true);
+            displayButton({980, 740}, {180, 80}, "Create room", true);
         }
     protected:
     private:
