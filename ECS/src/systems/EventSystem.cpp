b958785daa8a099f533f060a367ac249657b48ec
Jules <jules.gresset@epitech.eu>
2024-01-13 15:07:48 +0100

    [ADD] Scalable component for sprites

--- ECS/src/systems/EventSystem.cpp
+++ ECS/src/systems/EventSystem.cpp
@@ -30,14 +30,14 @@ void EventSystem::RunEvents(Coordinator &coordinator, AssetManager &assetManager
 				coordinator.AddComponent<Hitbox>(entity, {std::any_cast<float>(event._data[5]), std::any_cast<float>(event._data[6]), std::any_cast<float>(event._data[7]), std::any_cast<float>(event._data[8]), std::any_cast<HitboxType>(event._data[9])});
 				coordinator.AddComponent<HealthPoint>(entity, {std::any_cast<int>(event._data[11]), std::any_cast<int>(event._data[12])});
 				if (std::any_cast<HitboxType>(event._data[9]) == (PLAYER)) {
-					coordinator.AddComponent<Sprite>(entity, {assetManager.LoadTexture(assetManager._sprite[std::any_cast<int>(event._data[13])])});
+					coordinator.AddComponent<Sprite>(entity, {assetManager.LoadTexture(assetManager._sprite[std::any_cast<int>(event._data[13])]), 1});
 					if (std::any_cast<int>(event._data[10]) == static_cast<int>(entity))
 						coordinator.AddComponent<Movable>(entity, {NONE});
 				}
 				if (std::any_cast<HitboxType>(event._data[9]) == (BULLET))
-					coordinator.AddComponent<Sprite>(entity, {assetManager.LoadTexture(assetManager._sprite[std::any_cast<int>(event._data[13])])});
+					coordinator.AddComponent<Sprite>(entity, {assetManager.LoadTexture(assetManager._sprite[std::any_cast<int>(event._data[13])]), 1});
 				if (std::any_cast<HitboxType>(event._data[9]) == (ENNEMY))
-					coordinator.AddComponent<Sprite>(entity, {assetManager.LoadTexture(assetManager._sprite[std::any_cast<int>(event._data[13])])});
+					coordinator.AddComponent<Sprite>(entity, {assetManager.LoadTexture(assetManager._sprite[std::any_cast<int>(event._data[13])]), 1});
 				break;
 			} case Event::actions::MOVE: {
 				coordinator.GetComponent<Position>(event._entity)._x += coordinator.GetComponent<Velocity>(event._entity)._x;
